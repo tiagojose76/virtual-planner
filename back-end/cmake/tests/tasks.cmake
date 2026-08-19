845f16af37c63ec7d0d6c15bae286c1a6da7fbfa
isabe1ltenorio <isabel.tenorio@ufape.edu.br>
2026-08-19 18:01:54 -0300

    [P-15.2] Testes de TimeSlot, incluindo overlaps

--- back-end/cmake/tests/tasks.cmake
+++ back-end/cmake/tests/tasks.cmake
@@ -1,4 +1,11 @@
 # Testes do modulo Task. Registre aqui qualquer teste novo de Task.
-#
-# Exemplo:
-#   virtual_planner_add_test(task_test unit/domain/entities/task_test.cpp)
+gi
+virtual_planner_add_test(
+  date_test
+  unit/domain/value_objects/date_test.cpp
+)
+
+virtual_planner_add_test(
+  time_slot_test
+  unit/domain/value_objects/time_slot_test.cpp
+)
